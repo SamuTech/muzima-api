edc3e0bd32674582582e5479d12e48a305426b51
JavaDeveloper <samuelowino43@yahoo.com>
2017-05-12 12:04:26 +0100

    Added java doc entries for methods and class description

--- src/main/java/com/muzima/util/NetworkUtils.java
+++ src/main/java/com/muzima/util/NetworkUtils.java
@@ -8,10 +8,23 @@ import java.net.HttpURLConnection;
 import java.net.Proxy;
 import java.net.URL;
 
+/**
+ * Manages network calls include checking Http Connection establishment.
+ */
 public class NetworkUtils {
     private static final Logger logger = LoggerFactory.getLogger(NetworkUtils.class.getCanonicalName());
 
-    public static boolean isAddressReachable(String address, Proxy proxy, int timeout) {
+    /**
+     * Cheks if the chosen address on reachable for client http calls.i.e.
+     * Checks if connection is available before begining transactions.
+     *
+     * @param address -String
+     * @param proxy -Proxy Object
+     * @param timeout -int timeout
+     * @return boolean -isAddressReachable
+     */
+
+    public static boolean isAddressReachable(String address, Proxy proxy, Integer timeout) {
         boolean serverAvailable = false;
         try {
             HttpURLConnection connection = null;
@@ -36,20 +49,46 @@ public class NetworkUtils {
         return serverAvailable;
     }
 
-    public static boolean isAddressReachable(String address, int timeout){
+    /**
+     * TODO:Add brief method definition.
+     * @param address - String
+     * @param timeout - Integer
+     * @return boolean - isAddressReachable
+     */
+    public static boolean isAddressReachable(String address, Integer timeout){
         return isAddressReachable(address,null, timeout);
     }
 
+    /**
+     * 
+     * @param address - String
+     * @return boolean 
+     */
+    
     public static boolean isAddressReachable(final String address){
         return NetworkUtils.isAddressReachable(address, Constants.CONNECTION_TIMEOUT);
     }
 
+    /**
+     * 
+     * @param address - String
+     * @return HttpURLConnection 
+     * @throws IOException
+     */
     public static HttpURLConnection openConnection(String address)  throws IOException {
         URL url = new URL(address);
         return (HttpURLConnection)url.openConnection();
 
     }
 
+    /**
+     * 
+     * @param address - String 
+     * @param proxy - Proxy Object
+     * @return  HttpURLConnection Object
+     * @throws IOException
+     */
+    
     public static HttpURLConnection openConnection(String address, Proxy proxy)  throws IOException {
         URL url = new URL(address);
         return (HttpURLConnection)url.openConnection(proxy);
